b994e3f108ea63d4549cc34728fb40ef6c516e07
“gooptions” <becomeanoptionsseller@gmail.com>
2025-06-28 20:34:36 +0800

    Remove Login file/directory

---
+++ Login/Config.py
@@ -0,0 +1,23 @@
+# Settings for Combobox
+
+# MessageForm
+comboBoxSKCenterLib_SetAuthority = (
+    "正式環境", "正式環境SGX", "測試環境", "測試環境SGX"
+)
+
+# OrderForm
+comboBoxGetOrderReport = (
+    "1:全部", "2:有效", "3:可消", "4:已消", "5:已成", "6:失敗", "7:合併同價格", "8:合併同商品", "9:預約" 
+)
+comboBoxGetFulfillReport = (
+    "1:全部", "2:合併同書號", "3:合併同價格", "4:合併同商品", "5:T+1成交回報"
+)
+comboBoxSetMaxQtynMarketType =(
+     "0：TS(證券)", "1：TF(期貨)", "2：TO(選擇權)", "3：OS(複委託)", "4：OF(海外期貨)", "5：OO(海外選擇權)"
+)
+comboBoxSetMaxCountnMarketType =(
+     "0：TS(證券)", "1：TF(期貨)", "2：TO(選擇權)", "3：OS(複委託)", "4：OF(海外期貨)", "5：OO(海外選擇權)"
+)
+comboBoxUnlockOrder =(
+     "0：TS(證券)", "1：TF(期貨)", "2：TO(選擇權)", "3：OS(複委託)", "4：OF(海外期貨)", "5：OO(海外選擇權)"
+)
